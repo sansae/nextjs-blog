dfb5d7713273107de86805e70c91851f5fbce352
sansae <kentsaeteurn@gmail.com>
2023-10-12 15:23:08 -0700

    added Head component to add a title to page

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,8 +1,14 @@
+import Head from 'next/head';
+
 import Link from 'next/link';
 
 export default function FirstPost() {
   return (
     <div>
+      <Head>
+        <title>First Post</title>
+      </Head>
+
       <h1>First Post</h1>
 
       <h2>Go to <Link href="/">home page</Link></h2>
